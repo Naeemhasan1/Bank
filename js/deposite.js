06d0a51b76b12447c9da59fc4f5fb6d957a115a9
unknown <csnaeem4b@gmail.com>
2023-01-14 17:48:45 +0600

    deposit & balance

--- js/deposite.js
+++ js/deposite.js
@@ -1,2 +1,29 @@
-console.log ('deposit file');
-console . log ('kalke event handler sikhbo');
+document.getElementById('btn-deposit').addEventListener('click',function(){
+    const depositField = document.getElementById('deposit-field');
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
+    // console.log(newDepositAmount);
+    // clear field after click
+    depositField.value='';
+    // deposit total link
+    const depositTotal =document.getElementById('deposit-total');
+    // get previous deposit total
+    const previousDepositTotalString=depositTotal.innerText;
+    const previousDepositTotal=parseFloat(previousDepositTotalString);
+    
+    // set new deposit total
+    const newDepositTotal=previousDepositTotal+newDepositAmount;
+    depositTotal.innerText=newDepositTotal
+
+    // Balance total link
+    const balanceTotal =document.getElementById('balance-total');
+    // get previous balance total
+    const previousBalanceTotalString= balanceTotal.innerText;
+    const previousBalanceTotal=parseFloat(previousBalanceTotalString);
+    
+    // set new balance total
+    const newBalanceTotal=previousBalanceTotal+newDepositAmount;
+    balanceTotal.innerText=newBalanceTotal;
+    
+
+})
