6d9e92cf56eb98cef7d29eac8cb703cc47980a2b
unknown <csnaeem4b@gmail.com>
2023-01-15 01:26:13 +0600

    alert

--- js/withdraw.js
+++ js/withdraw.js
@@ -5,4 +5,43 @@ document.getElementById('btn-withdraw').addEventListener('click' ,function(){
     const newWithdrawAmaount=parseFloat(newWithdrawAmaountString);
     console.log(newWithdrawAmaount);
 
+    //clear field after click
+    withdrawField.value='';
+    // withdraw total link
+    const withdrawTotal =document.getElementById('withdraw-total');
+    //get  previous withdraw total
+    const previousWithdrawTotalString=withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+    
+
+
+    // set new withdraw total
+    const newWithdrawTotal=previousWithdrawTotal+newWithdrawAmaount;
+    withdrawTotal.innerText=newWithdrawTotal;
+
+    // balance total link
+    const balanceTotal=document.getElementById('balance-total');
+
+    // get previous balance total
+    const previousBalanceTotalString=balanceTotal.innerText;
+    const previousBalanceTotal=parseFloat(previousBalanceTotalString);
+
+    // set new balance total
+
+    
+
+    if (newWithdrawAmaount>previousBalanceTotal)
+    {
+        alert("Baper eto taka nai")
+    }
+    else
+    {
+         const newBalanceTotal=previousBalanceTotal-newWithdrawAmaount;
+       balanceTotal.innerText=newBalanceTotal;
+    
+    }
+
+
+
+
 }) 
